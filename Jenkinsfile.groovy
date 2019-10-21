b7a797cc1dd8c82792a979a86308d2b775ffadf6
Anna Malanova <anna.malanova@slalom.com>
2019-10-21 14:43:10 -0500

    change6

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,16 +1,16 @@
 #!/usr/bin/env groovy
 @Library('jenkins-library@master') _
 
-// pipeline {
-//     agent any
-//     stages {
-//         stage('Git Checkout') {
-//             steps {
-//             gitCheckout(
-//                 branch: "master",
-//                 url: "https://github.com/spring-projects/spring-petclinic.git"
-//             )
-//             }
-//     }
-//     }
-// }
+pipeline {
+    agent any
+    stages {
+        stage('Git Checkout') {
+            steps {
+            gitCheckout(
+                branch: "master",
+                url: "https://github.com/ane4ka0205/mybase-project.git"
+            )
+            }
+    }
+    }
+}
