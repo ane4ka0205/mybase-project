8689e84c8c94b534dcac0b8b72f374f386a07e72
Anna Malanova <anna.malanova@slalom.com>
2019-10-21 15:53:30 -0500

    change7

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,9 @@
 #!/usr/bin/env groovy
 @Library('jenkins-library@master') _
 
-node {https://github.com/ane4ka0205/jenkins-library.git
+// node {https://github.com/ane4ka0205/jenkins-library.git
 
-gitCheckout.printHello()
-}
+// gitCheckout.printHello()
+// }
+
+gitCheckout.(currentBuild.getNumber())
