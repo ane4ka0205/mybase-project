864ef93196b0f67bb627e369ba83b5035d1e6c12
Anna Malanova <anna.malanova@slalom.com>
2019-10-21 18:44:16 -0500

    change5

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,13 +1,34 @@
 #!/usr/bin/env groovy
 @Library('jenkins-library@master') _
 
-node {https://github.com/ane4ka0205/jenkins-library.git
+node {
+    timedCommand.cmd = 'ls -la'
+    echo timedCommand.cmd
+    timedCommand.runCommand()
+    echo timedCommand.getOutput()
+    //https://github.com/ane4ka0205/jenkins-library.git
 
-    stage('hello'){
-        gitCheckout.printHello()
+    // stage('stage'){
+    //     gitCheckout.printHello()
+    // }
+    // stage('bye'){
+    //     gitCheckout.printBye()
+    // }
+
+    stage('stage'){
+        gitCheckout.setCommand
     }
-    stage('bye'){
-        gitCheckout.printBye()
+
+    stage('stage'){
+        gitCheckout.getCommand
+    }
+
+    stage('stage'){
+        gitCheckout.runCommand
+    }
+
+    stage('stage'){
+        gitCheckout.getOutput
     }
 }
 
