8f8fb3d417f96f2a1bf0a95ea8d9607a85cbbba0
Anna Malanova <anna.malanova@slalom.com>
2019-10-21 15:00:51 -0500

    change7

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,16 +1,5 @@
 #!/usr/bin/env groovy
 @Library('jenkins-library@master') _
 
-pipeline {
-    agent any
-    stages {
-        stage('Git Checkout') {
-            steps {
-            gitCheckout(
-                branch: "master",
-                url: "https://github.com/ane4ka0205/mybase-project.git"
-            )
-            }
-    }
-    }
-}
+def common = new com.example.Pipeline()
+common.runPipeline()
