3576947ffa6a963e39c87628b35b8e0035c17287
Anna Malanova <anna.malanova@slalom.com>
2019-10-21 15:17:49 -0500

    change2

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,3 +1,16 @@
 #!/usr/bin/env groovy
 @Library('jenkins-library@master') _
 
+pipeline {
+    agent any
+    stages {
+        stage('Git Checkout') {
+            steps {
+            gitCheckout(
+                branch: "master",
+                url: "https://github.com/ane4ka0205/mybase-project.git"
+            )
+            }
+    }
+    }
+}
