d98969ba54bf1cda77355cde9faf1d9b915e99d5
Anna Malanova <anna.malanova@slalom.com>
2019-10-21 19:09:27 -0500

    change4

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
-@Library('jenkins-library@master') import static com.example.buildUtils 
+@Library('jenkins-library@master') _
 
 node {
     https://github.com/ane4ka0205/jenkins-library.git
